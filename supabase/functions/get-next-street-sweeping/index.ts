785d00302f1b01c48a92d1828b3608ab2fc07472
Joseph Park <vapor47@gmail.com>
2026-04-07 01:29:25 -0700

    Wire get-next-street-sweeping business logic to supabase edge function

--- supabase/functions/get-next-street-sweeping/index.ts
+++ supabase/functions/get-next-street-sweeping/index.ts
@@ -9,34 +9,11 @@ import { getNextStreetSweeping } from "./get-next-street-sweeping.ts";
 console.log("Hello from Functions!")
 
 Deno.serve(async (req) => {
-  const { now, restrictions } = await req.json()
-
-  /*
-  From the provided `now` timestamp, find the next upcoming street sweeping restriction from the `street_sweeping_restrictions` array and return it in the response.
-  
-  now:
-    curr_weekday,
-    curr_time,
-    curr_week_of_month
-
-  check next week of month starting from current week
-
-
-  get first week of month where week_of_month >= curr_week_of_month and 
-
-  if we can numerate restriction days, then we could just compare numerically.
-  say our 1st day is a thursday, and our restriction is on the 2nd and 4th wednesdays - then we can know that our first wednesday is +6 days == 7.
-  Therefore 2nd and 4th wednesdays are +13 and +27 days respectively (14, 28). (Consider overflow into next month).
-  */
-
-
-
-  const data = {
-    message: `Hello ${name}!`,
-  }
+  const { now, sweeping_schedule } = await req.json()
+  const nextSweeping = getNextStreetSweeping(new Date(now), sweeping_schedule)
 
   return new Response(
-    JSON.stringify(data),
+    JSON.stringify( nextSweeping ),
     { headers: { "Content-Type": "application/json" } },
   )
 })
@@ -49,6 +26,6 @@ Deno.serve(async (req) => {
   curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/get-next-street-sweeping' \
     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
+    --data '{"now":"2026-04-06T07:00:00","sweeping_schedule":{"weekday":"Tue","from_time":"09:00","to_time":"11:00","weeks":[true,false,false,false,false],"holidays":false}}'
 
 */
